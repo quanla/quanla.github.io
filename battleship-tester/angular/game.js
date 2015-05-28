668e2ab72db07346b4c741ba180525c5f338acee
Le Anh Quan <quanla2003@gmail.com>
2015-05-28 16:19:25 +0700

    Fix attack return method

--- battleship-tester/angular/game.js
+++ battleship-tester/angular/game.js
@@ -19,13 +19,13 @@
             }
 
             var shipTypes = [
-                { name: "6", length: 1 },
-                { name: "5", length: 1 },
-                { name: "4", length: 2 },
-                { name: "3", length: 2 },
-                { name: "2", length: 3 },
-                { name: "1", length: 4 },
-                { name: "0", length: 5 }
+                { no: 6, length: 1 },
+                { no: 5, length: 1 },
+                { no: 4, length: 2 },
+                { no: 3, length: 2 },
+                { no: 2, length: 3 },
+                { no: 1, length: 4 },
+                { no: 0, length: 5 }
             ];
 
             function createShips() {
@@ -35,7 +35,7 @@
                     for (var i = 0; i < shipTypes.length; i++) {
                         var shipType = shipTypes[i];
 
-                        var ship = createShip(shipType.name, shipType.length, ships);
+                        var ship = createShip(shipType.no, shipType.length, ships);
                         if (ship != null) {
                             ships.push(ship);
                         } else {
@@ -47,12 +47,12 @@
                 }
             }
 
-            function createShip(name, length, ships) {
+            function createShip(no, length, ships) {
                 for (var i=0;i < 100; i++) {
                     var start = randomPoint();
                     var direction = Math.floor(Math.random() * 4);
 
-                    var ship = new Ship(name, start, length, direction);
+                    var ship = new Ship(no, start, length, direction);
 
                     if (ship.canPlace(ships)) {
                         return ship;
@@ -77,6 +77,41 @@
                     var stashedAttacks = [];
 
                     var ships = createShips(10, 10);
+
+                    function checkAttack(x, y) {
+                        for (var i = 0; i < ships.length; i++) {
+                            var ship = ships[i];
+                            if (ship.on(x, y)) {
+                                if (isSank(ship)) {
+                                    return {
+                                       "no": ship.no,        // Piece number
+                                       "kind": ['carrier', 'warship', 'submarine', 'cruiser','patrol'][5 - ship.length],      // .. kind: 'carrier', 'warship', 'submarine', 'cruiser' or 'patrol'
+                                       "size": ship.length,       // .. size: [1,5]
+                                       "x": ship.start.x,          // .. x coordinate: [0,10)
+                                       "y": ship.start.y,          // .. y coordinate: [0,10)
+                                       "direction": ship.direction == 0 ? "v" : "h" // .. direction: 'h' or 'w'
+                                    };
+                                } else {
+                                    return true;
+                                }
+                            }
+                        }
+                        return false;
+                    }
+
+                    function attacked(x, y) {
+                        return !!Cols.find(attacks, function(a) {
+                            return a.x == x && a.y == y;
+                        });
+                    }
+
+                    function isSank(ship) {
+                        return !ship.eachPoint(function(x, y) {
+                            // Find a point which is not attacked
+                            return !attacked(x, y);
+                        });
+                    }
+
                     return {
                         rows: createRows(),
                         prevTurn: function() {
@@ -87,9 +122,19 @@
                         ships: ships,
                         nextTurn: function() {
                             if (Cols.isEmpty(stashedAttacks)) {
+                                var called = false;
                                 var turn = {
                                     attack: function(x, y) {
+                                        if (called) {
+                                            throw "Hey, you called to attack this turn already";
+                                        }
+                                        called = true;
+
                                         attacks.push({x: x, y: y});
+
+                                        var ret = checkAttack(x, y);
+
+                                        return ret;
                                     }
                                 };
                                 bot.play(turn);
@@ -142,11 +187,22 @@
         return false;
     }
 
-    var Ship = function(name, start, length, direction) {
-        this.name = name;
+    var Ship = function(no, start, length, direction) {
+        // Not use direction 1, 2
+        if (direction == 1) {
+            direction = 3;
+            start.x -= length - 1;
+        }
+        if (direction == 2) {
+            direction = 0;
+            start.y -= length - 1;
+        }
+
+        this.no = no;
         this.start = start;
         this.length = length;
         this.direction = direction;
+
     };
     Ship.prototype = {
         on: function(x, y) {
@@ -162,27 +218,35 @@
             }
             return false;
         },
-        canPlace: function(ships) {
+        eachPoint: function(funcXY) {
 
             var p = ObjectUtil.clone(this.start);
             var directionChange = directionChanges[this.direction];
 
             for (var i = 0; i < this.length; i++) {
-                if (p.x < 0 || p.x >= 10
-                    || p.y < 0 || p.y >= 10) {
-                    return false;
+                if (funcXY(p.x, p.y)) {
+                    return true;
+                }
+                directionChange(p);
+            }
+            return false;
+        },
+        canPlace: function(ships) {
+            return !this.eachPoint(function(x, y) {
+
+                if (x < 0 || x >= 10
+                    || y < 0 || y >= 10) {
+                    return true;
                 }
 
                 for (var j = 0; j < ships.length; j++) {
                     var ship = ships[j];
-                    if (findAround(p.x, p.y, function(x, y) { return ship.on(x, y); })) {
-                        return false;
+                    if (findAround(x, y, function(x1, y1) { return ship.on(x1, y1); })) {
+                        return true;
                     }
                 }
-
-                directionChange(p);
-            }
-            return true;
+                return false;
+            });
         }
     };
 })();
